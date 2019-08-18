f5c34ca45460bccf74825a4db44ce4fcdcc42af1
Alexey Golub <Tyrrrrrr@gmail.com>
2019-08-18 13:23:15 +0300

    Use invariant culture in CliFx.Tests.Dummy

--- CliFx.Tests.Dummy/Program.cs
+++ CliFx.Tests.Dummy/Program.cs
@@ -1,14 +1,21 @@
-﻿using System.Threading.Tasks;
+﻿using System.Globalization;
+using System.Threading.Tasks;
 
 namespace CliFx.Tests.Dummy
 {
     public static class Program
     {
-        public static Task<int> Main(string[] args) =>
-            new CliApplicationBuilder()
+        public static Task<int> Main(string[] args)
+        {
+            // Set culture to invariant to maintain consistent format because we rely on it in tests
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+            return new CliApplicationBuilder()
                 .WithCommandsFromThisAssembly()
                 .UseDescription("Dummy program used for E2E tests.")
                 .Build()
                 .RunAsync(args);
+        }
     }
 }
