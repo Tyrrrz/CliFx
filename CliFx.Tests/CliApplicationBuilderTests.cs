b70b25076e7060d3bf7d858956cac00a99ee8295
Alexey Golub <Tyrrrrrr@gmail.com>
2019-08-24 18:31:17 +0300

    Add smoke tests for CliApplicationBuilder

---
+++ CliFx.Tests/CliApplicationBuilderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using CliFx.Services;
+using NUnit.Framework;
+
+namespace CliFx.Tests
+{
+    [TestFixture]
+    public partial class CliApplicationBuilderTests
+    {
+        [Test]
+        public void Build_Smoke_Test()
+        {
+            // Just test that application can be built after calling all methods
+
+            // Arrange
+            var builder = new CliApplicationBuilder();
+
+            // Act
+            builder
+                .AddCommand(typeof(TestCommand))
+                .AddCommandsFrom(typeof(TestCommand).Assembly)
+                .AddCommands(new[] {typeof(TestCommand)})
+                .AddCommandsFrom(new[] {typeof(TestCommand).Assembly})
+                .AddCommandsFromThisAssembly()
+                .AllowDebugMode()
+                .AllowPreviewMode()
+                .UseTitle("test")
+                .UseExecutableName("test")
+                .UseVersionText("test")
+                .UseDescription("test")
+                .UseConsole(new VirtualConsole(TextWriter.Null))
+                .UseCommandFactory(schema => (ICommand) Activator.CreateInstance(schema.Type))
+                .Build();
+        }
+    }
+}
