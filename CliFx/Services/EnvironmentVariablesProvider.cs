3318724f52c03145e4ee2761717ec2e2d316c114
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2019-09-20 16:30:05 +0200

    EnvironmentVariablesProvider implemented

---
+++ CliFx/Services/EnvironmentVariablesProvider.cs
@@ -0,0 +1,53 @@
+﻿using CliFx.Internal;
+using System;
+using System.Collections.Generic;
+using System.Security;
+
+namespace CliFx.Services
+{
+    /// <inheritdoc />
+    public class EnvironmentVariablesProvider : IEnvironmentVariablesProvider
+    {
+        /// <summary>
+        /// Default delimiter for multiple values
+        /// </summary>
+        public const char DEFAULT_VALUES_DELIMITER = ',';
+
+        private readonly char _valuesDelimiter;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="EnvironmentVariablesProvider"/> with the values delimiter specified
+        /// </summary>
+        public EnvironmentVariablesProvider(char valuesDelimiter)
+        {
+            _valuesDelimiter = valuesDelimiter;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="EnvironmentVariablesProvider"/> with the default values delimiter
+        /// </summary>
+        public EnvironmentVariablesProvider()
+            : this(DEFAULT_VALUES_DELIMITER)
+        {
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<string> GetValues(string variableName)
+        {
+            variableName.GuardNotNull(nameof(variableName));
+
+            try
+            {
+                string variableValue = Environment.GetEnvironmentVariable(variableName);
+
+                if (string.IsNullOrWhiteSpace(variableValue)) return null;
+
+                return variableValue.Split(_valuesDelimiter);
+            }
+            catch (SecurityException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ CliFx/Services/IEnvironmentVariablesProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace CliFx.Services
+{
+    /// <summary>
+    /// Provides environment variable values
+    /// </summary>
+    public interface IEnvironmentVariablesProvider
+    {
+        /// <summary>
+        /// Returns one or more values for the environment variable provided, otherwise returns null.
+        /// </summary>
+        /// <remarks>If the User is not allowed to read environment variables it will return null.</remarks>
+        IEnumerable<string> GetValues(string variableName);
+    }
+}
