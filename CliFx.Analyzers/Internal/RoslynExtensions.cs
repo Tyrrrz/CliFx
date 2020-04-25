c68235ec29fddcba1177b07c95030fbe52e21e07
Alexey Golub <tyrrrz@gmail.com>
2020-04-25 17:55:43 +0300

    wip

--- CliFx.Analyzers/Internal/RoslynExtensions.cs
+++ CliFx.Analyzers/Internal/RoslynExtensions.cs
@@ -7,8 +7,5 @@ namespace CliFx.Analyzers.Internal
     {
         public static bool DisplayNameMatches(this ISymbol symbol, string name) =>
             string.Equals(symbol.ToDisplayString(), name, StringComparison.Ordinal);
-
-        public static bool DisplayNameStartsWith(this ISymbol symbol, string name) =>
-            symbol.ToDisplayString().StartsWith(name, StringComparison.Ordinal);
     }
 }
