d32d6054d3c4dce5e9cfd46b4d02c7f844fa9d95
mauricel <maurice.leong@gmail.com>
2021-03-08 16:13:57 +0000

    Add tests: [suggest] sub command suggestions.

--- CliFx.Tests/SuggestionSpecs.cs
+++ CliFx.Tests/SuggestionSpecs.cs
@@ -16,18 +16,55 @@ namespace CliFx.Tests
         private readonly ITestOutputHelper _output;
 
         public SuggestionSpecs(ITestOutputHelper output) => _output = output;
+
         [Theory]
         [InlineData(new[] { "[suggest]" }, new[] { "named" })]
         [InlineData(new[] { "[suggest]", "n" }, new[] { "named" })]
         [InlineData(new[] { "[suggest]", "named" }, new[] { "" })]
         [InlineData(new[] { "[suggest]", "named_badly" }, new string[] { "" })]
-        public async Task Suggestion_directive_returns_good_command_suggestions(string[] input, string[] expected)
+        public async Task Suggestion_directive_returns_command_suggestions(string[] input, string[] expected)
         {
             // Arrange
             var (console, stdOut, _) = VirtualConsole.CreateBuffered();
 
             var application = new CliApplicationBuilder()
                 .AddCommand<NamedCommand>()
+                .UseConsole(console)
+                .AllowSuggestMode()
+                .Build();
+
+            // Act
+            var exitCode = await application.RunAsync(
+                input,
+                new Dictionary<string, string>()
+            );
+
+            // Assert
+            exitCode.Should().Be(0);
+            var outputSet = stdOut.GetString().Split("\n").ToHashSet();
+            var expectedSet = expected.ToHashSet();
+
+            outputSet.Should().BeEquivalentTo(expectedSet);
+
+            _output.WriteLine(stdOut.GetString());
+        }
+
+
+        [Theory]
+        [InlineData(new[] { "[suggest]" }, new[] { "named", "named sub" })]
+        [InlineData(new[] { "[suggest]", "n" }, new[] { "named", "named sub" })]
+        [InlineData(new[] { "[suggest]", "named" }, new[] { "" })]
+        [InlineData(new[] { "[suggest]", "named_badly" }, new string[] { "" })]
+        [InlineData(new[] { "[suggest]", "named s" }, new string[] { "named sub" })]
+        [InlineData(new[] { "[suggest]", "named sub" }, new string[] { "" })]
+        public async Task Suggestion_directive_returns_subcommand_suggestions(string[] input, string[] expected)
+        {
+            // Arrange
+            var (console, stdOut, _) = VirtualConsole.CreateBuffered();
+
+            var application = new CliApplicationBuilder()
+                .AddCommand<NamedCommand>()
+                .AddCommand<NamedSubCommand>()
                 .UseConsole(console)
                 .AllowSuggestMode()
                 .Build();
