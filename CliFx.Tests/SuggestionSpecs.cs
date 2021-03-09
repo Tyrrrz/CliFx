1235e78277fdd3d253962cf453792544e7f7d9fe
mauricel <maurice.leong@gmail.com>
2021-03-09 23:46:48 +0000

    Fix tests: make sure expected inputs match observed powershell behaviour

--- CliFx.Tests/SuggestionSpecs.cs
+++ CliFx.Tests/SuggestionSpecs.cs
@@ -19,9 +19,9 @@ namespace CliFx.Tests
 
         [Theory]
         [InlineData(new[] { "[suggest]", "app-placeholder.exe" }, new[] { "named" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "n" }, new[] { "named" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named" }, new string[] { })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named_badly" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe n" }, new[] { "named" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named_badly" }, new string[] { })]
         public async Task Suggestion_directive_returns_command_suggestions(string[] input, string[] expected)
         {
             // Arrange
@@ -52,11 +52,11 @@ namespace CliFx.Tests
 
         [Theory]
         [InlineData(new[] { "[suggest]", "app-placeholder.exe" }, new[] { "named", "named sub" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "n" }, new[] { "named", "named sub" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named" }, new string[] {})]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named_badly" }, new string[] {})]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named s" }, new string[] { "named sub" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "named sub" }, new string[] {})]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe n" }, new[] { "named", "named sub" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named" }, new string[] {})]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named_badly" }, new string[] {})]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named s" }, new string[] { "named sub" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe named sub" }, new string[] {})]
         public async Task Suggestion_directive_returns_subcommand_suggestions(string[] input, string[] expected)
         {
             // Arrange
@@ -116,13 +116,13 @@ namespace CliFx.Tests
 
         [Theory]
         // [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", " " }, new string[] { "Value1", "Value2", "Value3"})] // spaces eaten by Environment.GetCommandLineArgs, can't implement.
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "v" }, new string[] { "Value", "Value1", "Value2", "Value3" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value" }, new string[] { "Value", "Value1", "Value2", "Value3" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value4" }, new string[] {  })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value", "c" }, new string[] { "Custom", "Custom1", "Custom2", "Custom3" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value", "Custom4" }, new string[] { })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value", "Custom4", "a" }, new string[] { })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "Value", "Custom4", "a", "b" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd v" }, new string[] { "Value", "Value1", "Value2", "Value3" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value" }, new string[] { "Value", "Value1", "Value2", "Value3" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value4" }, new string[] {  })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value c" }, new string[] { "Custom", "Custom1", "Custom2", "Custom3" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value Custom4" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value Custom4 a" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd Value Custom4 a b" }, new string[] { })]
         public async Task Suggestion_directive_suggests_enum_parameters(string[] input, string[] expected)
         {
             // Arrange
@@ -151,13 +151,13 @@ namespace CliFx.Tests
         }
 
         [Theory]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "-" }, new string[] { "-h", "--help" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "--" }, new string[] { "--help" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "-" }, new string[] { "-a", "-b", "-c", "-h", "--opt-aa", "--opt-bb", "--opt-cc", "--help" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "--" }, new string[] { "--opt-aa", "--opt-bb", "--opt-cc", "--help" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "--opt-a" }, new string[] { "--opt-aa" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "--opt-aa" }, new string[] { "--opt-aa" })]
-        [InlineData(new[] { "[suggest]", "app-placeholder.exe", "cmd", "--opt-d" }, new string[] { })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe -" }, new string[] { "-h", "--help" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe --" }, new string[] { "--help" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd -" }, new string[] { "-a", "-b", "-c", "-h", "--opt-aa", "--opt-bb", "--opt-cc", "--help" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd --" }, new string[] { "--opt-aa", "--opt-bb", "--opt-cc", "--help" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd --opt-a" }, new string[] { "--opt-aa" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd --opt-aa" }, new string[] { "--opt-aa" })]
+        [InlineData(new[] { "[suggest]", "app-placeholder.exe cmd --opt-d" }, new string[] { })]
         public async Task Suggestion_directive_suggests_options(string[] input, string[] expected)
         {
             // Arrange
