31ae0271b99acb1fa48522fdfa0c97b737615b3b
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-02-11 21:42:33 +0200

    Cleanup

--- CliFx.Tests.Dummy/Program.cs
+++ CliFx.Tests.Dummy/Program.cs
@@ -3,12 +3,13 @@ using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CliFx.Tests.Dummy;
+
 // This dummy application is used in tests for scenarios
 // that require an external process to properly verify.
 
 public static partial class Program
 {
-    public static Assembly Assembly { get; } = typeof(Program).Assembly;
+    public static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
 
     public static string Location { get; } = Assembly.Location;
 }
